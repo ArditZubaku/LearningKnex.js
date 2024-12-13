368bcde881fd36bb3890d6d2f1c3ae732fb06211
ArditZubaku <zubaku92@gmail.com>
2024-12-13 15:06:19 +0100

    Create operations.

--- src/examples/crud.ts
+++ src/examples/crud.ts
@@ -1,4 +1,4 @@
-import {Author, Book} from "../types";
+import {Author, Book, Genre} from "../types";
 import knex from "../config/knex";
 
 export const getAllAuthors = async (limit: number, offset: number): Promise<Author[]> => {
@@ -16,3 +16,39 @@ export async function getAuthorById(id: number): Promise<Author | undefined> {
     return knex("authors").where({id}).first();
 }
 
+export async function getGenreById(id: number) {
+    return knex("genres").where({id}).first();
+}
+
+export async function createAuthor({name, bio}: Partial<Author>): Promise<Pick<Author, "id" | "name"> | undefined> {
+    // return knex("authors").insert({name, bio}, "*").first();
+    return knex("authors").insert({name, bio}, ["id", "name"]);
+}
+
+export async function createGenre(genre: Partial<Genre>) {
+    // return knex("genres").insert(genre, "*");
+    // return knex("genres").insert(genre, "*").returning("*").then(rows => rows[0]);
+    return (await knex("genres").insert(genre, "*"))[0];
+}
+
+async function checkIfAuthorExists(id?: number) {
+    if (!id) throw new Error("Author ID is required!");
+
+    const t1 = performance.now();
+    const author = await getAuthorById(id);
+    const t2 = performance.now();
+    console.log("This query took: ", t2 - t1, "ms");
+    if (!author) throw new Error("Invalid ID!");
+}
+
+async function checkIfGenreExists(id?: number) {
+    if (!id) throw new Error("Genre ID is required!");
+
+    const genre = await getGenreById(id);
+    if (!genre) throw new Error("Invalid ID");
+}
+
+export async function createBook(book: Partial<Book>): Promise<Book | undefined> {
+    await Promise.all([checkIfAuthorExists(book.author_id), checkIfGenreExists(book.genre_id)]);
+    return (await knex("books").insert(book, "*")).at(0);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import {error, log} from "console";
 import {onDatabaseConnect} from "./config/knex";
-import {getAllAuthors, getAllBooks, getAuthorById} from "./examples/crud";
+import {createAuthor, createBook, createGenre, getAllAuthors, getAllBooks, getAuthorById} from "./examples/crud";
 import {faker} from "@faker-js/faker/locale/en";
+import {randomUUID} from "node:crypto";
 
 onDatabaseConnect()
     .then(async () => {
         log(await getAllAuthors(2, 0));
         log(await getAllBooks(3, 2));
         log(await getAuthorById(faker.number.int({min: 1, max: 100})));
+        log(await createAuthor({name: faker.person.firstName("male"), bio: faker.person.bio()}))
+        log(await createGenre({name: faker.book.genre() + randomUUID()}));
+        log(await createBook({
+            title: faker.book.title(),
+            price: faker.number.int({max: 10_000}),
+            description: faker.lorem.paragraph(3),
+            genre_id: 10,
+            author_id: 10,
+        }))
     })
     .catch((err) => error(err));
